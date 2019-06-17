9e39c1945b096381bd8265103db73132895168ca
TALIBART Hugo <hugo.talibart@irisa.fr>
2019-06-17 09:09:32 +0200

    output folder instead of aln and info res file

--- compotts_wrapper/__main__.py
+++ compotts_wrapper/__main__.py
@@ -1,7 +1,7 @@
 import argparse
 
 from call_compotts import *
-
+import time
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -11,22 +11,20 @@ if __name__ == '__main__':
     parser.add_argument('-s2', '--sequence_file_2', help="Sequence file 2")
     parser.add_argument('-h1', '--a3m_file_1', help="HH-blits output file 1")
     parser.add_argument('-h2', '--a3m_file_2', help="HH-blits output file 2")
-    parser.add_argument('-a', '--aln_res_file', help="Output alignment file")
-    parser.add_argument('-i', '--info_res_file', help="Output info file")
+    parser.add_argument('-o', '--output_folder', help="Output folder")
     args = vars(parser.parse_args())
 
 
-    aln_res_file = args["aln_res_file"]
-    info_res_file = args["info_res_file"]
+    output_folder = args["output_folder"]
 
-    if aln_res_file is None:
-        aln_res_file = "aln.csv"
-    if info_res_file is None:
-        info_res_file = "info.csv"
-    
+    if output_folder is None:
+        output_folder = time.strftime("%Y%m%d-%H%M%S")
+
+    create_folder(output_folder)
+
     if (args["potts_model_1"] is not None) and (args["potts_model_2"] is not None):
-        align_two_potts_models_from_files([args["potts_model_1"], args["potts_model_2"]], aln_res_file, info_res_file)
+        align_two_potts_models_from_files([args["potts_model_1"], args["potts_model_2"]], output_folder)
     elif (args["sequence_file_1"] is not None) and (args["sequence_file_2"] is not None) and (args["a3m_file_1"] is not None) and (args["a3m_file_2"] is not None):
-        align_hhblits_output([args["sequence_file_1"],args["sequence_file_2"]], [args["a3m_file_1"],args["a3m_file_2"]], aln_res_file, info_res_file)
+        align_hhblits_output([args["sequence_file_1"],args["sequence_file_2"]], [args["a3m_file_1"],args["a3m_file_2"]], output_folder)
     elif (args["sequence_file_1"] is not None) and (args["sequence_file_2"] is not None):
-        align_one_hot([args["sequence_file_1"],args["sequence_file_2"]], aln_res_file, info_res_file)
+        align_one_hot([args["sequence_file_1"],args["sequence_file_2"]], output_folder)
--- compotts_wrapper/call_compotts.py
+++ compotts_wrapper/call_compotts.py
@@ -11,7 +11,10 @@ INFINITY = 10000000
 # TODO stocker les scores en utilisant la symétrie pour prendre moins de mémoire
 # TODO aln_res_file et info_res_file comme sortie de ComPotts -> variables
 
-def align_two_potts_models(mrfs, aln_res_file, info_res_file, n_limit_param=INFINITY, iter_limit_param=INFINITY, t_limit=36000, disp_level=1, epsilon=1, v_score_function=scalar_product, w_score_function=scalar_product, gap_open=0, gap_extend=0, w_threshold=0, **kwargs):
+def align_two_potts_models(mrfs, output_folder, n_limit_param=INFINITY, iter_limit_param=INFINITY, t_limit=36000, disp_level=1, epsilon=1, v_score_function=scalar_product, w_score_function=scalar_product, gap_open=0, gap_extend=0, w_threshold=0, **kwargs):
+
+    aln_res_file = output_folder+"aln.csv"
+    info_res_file = output_folder+"info.csv"
 
     v_scores = compute_v_scores(*mrfs, v_score_function)
     c_v_scores = ctypes.c_void_p(v_scores.ctypes.data)
@@ -30,24 +33,24 @@ def align_two_potts_models(mrfs, aln_res_file, info_res_file, n_limit_param=INFI
     COMPOTTS_SOLVER.call_from_python(c_v_scores, c_w_scores, ctypes.c_int(mrfs[0].ncol), ctypes.c_int(mrfs[1].ncol), c_edges_mapA, c_edges_mapB, ctypes.c_double(selfcompA), ctypes.c_double(selfcompB), ctypes.c_double(gap_open), ctypes.c_double(gap_extend), ctypes.c_char_p(aln_res_file.encode('utf-8')), ctypes.c_char_p(info_res_file.encode('utf-8')), ctypes.c_int(iter_limit_param), ctypes.c_int(t_limit), ctypes.c_int(disp_level), ctypes.c_double(epsilon))
 
 
-def align_two_potts_models_from_files(mrf_files, aln_res_file, info_res_file, **kwargs):
+def align_two_potts_models_from_files(mrf_files, output_folder, **kwargs):
     mrfs = [Potts_Model.from_msgpack(mrf_file) for mrf_file in mrf_files]
-    align_two_potts_models(mrfs, aln_res_file, info_res_file, **kwargs)
+    align_two_potts_models(mrfs, output_folder, **kwargs)
 
 
-def align_two_objects(objects, aln_res_file, info_res_file, **kwargs):
-    align_two_potts_models([o.mrf for o in objects], aln_res_file, info_res_file, **kwargs)
+def align_two_objects(objects, output_folder, **kwargs):
+    align_two_potts_models([o.mrf for o in objects], output_folder, **kwargs)
 
 
-def align_hhblits_output(seq_files, a3m_files, aln_res_file, info_res_file, **kwargs):
+def align_hhblits_output(seq_files, a3m_files, output_folder, **kwargs):
     objects = []
     for seq_file, a3m_file in zip(seq_files, a3m_files):
         objects.append(ComPotts_Object.from_hhblits_output(seq_file, a3m_file, **kwargs))
-    align_two_objects(objects, aln_res_file, info_res_file, **kwargs)
+    align_two_objects(objects, output_folder, **kwargs)
 
 
-def align_one_hot(seq_files, aln_res_file, info_res_file, **kwargs):
+def align_one_hot(seq_files, output_folder, **kwargs):
     objects = []
     for seq_file in seq_files :
         objects.append(ComPotts_Object.from_seq_file_to_one_hot(seq_file))
-    align_two_objects(objects, aln_res_file, info_res_file, **kwargs)
+    align_two_objects(objects, output_folder, **kwargs)
