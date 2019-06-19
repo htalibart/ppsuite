82ff0065f8c1d1c2342d065f453805c1fd2e15b1
TALIBART Hugo <hugo.talibart@irisa.fr>
2019-06-19 09:24:14 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='VAPotts',
     version='0.1dev',
-    packages=['compotts','vizpotts'],
+    packages=find_packages(),
     long_description=open('README.md').read(),
 )
