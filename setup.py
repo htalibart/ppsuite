93f84194c707caf0401ada36dea4f9e619942699
TALIBART Hugo <hugo.talibart@irisa.fr>
2019-06-20 17:33:57 +0200

    install_requires

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(
     name='VAPotts',
     version='0.1dev',
     packages=find_packages(),
+    install_requires=['numpy', 'pandas', 'biopython'], # TODO
     long_description=open('README.md').read(),
 )
