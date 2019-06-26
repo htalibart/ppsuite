1229a2fa64be6e086131865c668b6aea92824282
TALIBART Hugo <hugo.talibart@irisa.fr>
2019-06-26 10:50:18 +0200

    default mode : hhblits

--- compotts/__main__.py
+++ compotts/__main__.py
@@ -30,7 +30,7 @@ def main():
     parser.add_argument('-r', '--rescaling_function', help="Rescaling function for Potts model parameters.", default="identity", choices=('identity', 'original_rescaling', 'symmetric_relu_like'))
     parser.add_argument('-nw', '--no_w', help="Don't use w scores", action='store_true')
     parser.add_argument('-wt', '--w_threshold_method', help="w threshold method. Couplings that have a Frobenius norm below the threshold are not considered by ComPotts", default="no_threshold") # TODO checker si c'est bien fait avant le rescaling
-    parser.add_argument('-m', '--mode', help="Mode", choices=('msgpack', 'hhblits', 'one_hot', 'one_seq_ccmpred'), default='one_seq_ccmpred')
+    parser.add_argument('-m', '--mode', help="Mode", choices=('msgpack', 'hhblits', 'one_hot', 'one_seq_ccmpred'), default='hhblits')
     args = vars(parser.parse_args())
 
 
