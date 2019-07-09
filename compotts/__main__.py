d4184a1cc27a5fc41afd511a325b778a35985790
TALIBART Hugo <hugo.talibart@irisa.fr>
2019-07-09 14:04:12 +0200

    gap open is 5 by default

--- compotts/__main__.py
+++ compotts/__main__.py
@@ -26,7 +26,7 @@ def main(args=sys.argv[1:]):
     parser.add_argument('-r', '--rescaling_function', help="Rescaling function for Potts model parameters.", default="identity", choices=('identity', 'original_rescaling', 'symmetric_relu_like', 'shifted_relu'))
     parser.add_argument('-nw', '--no_w', help="Don't use w scores", action='store_true')
     parser.add_argument('-wt', '--w_threshold_method', help="w threshold method. Couplings that have a Frobenius norm below the threshold are not considered by ComPotts", default="no_threshold") # TODO checker si c'est bien fait avant le rescaling
-    parser.add_argument('-go', '--gap_open', help="gap open", type=float, default=0)
+    parser.add_argument('-go', '--gap_open', help="gap open", type=float, default=5)
     parser.add_argument('-ge', '--gap_extend', help="gap extend", type=float, default=0)
     parser.add_argument('-m', '--mode', help="Mode", choices=('msgpack', 'hhblits', 'one_hot', 'one_seq_submat', 'one_seq_ccmpred'), default='hhblits')
     parser.add_argument('-ali', '--call_aliview', help="Call aliview at the end", action='store_true')
