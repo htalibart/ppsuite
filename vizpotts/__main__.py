fb775375875cfc4b693fa98ab16aeb8587856891
TALIBART Hugo <hugo.talibart@irisa.fr>
2019-07-12 11:03:28 +0200

    better vizpotts arguments

--- vizpotts/__main__.py
+++ vizpotts/__main__.py
@@ -7,7 +7,7 @@ from basic_modules.potts_model import *
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-p', '--potts_model', help="Potts model (msgpack file)", type=pathlib.Path)
+    parser.add_argument('-p', '--potts_models', help="Potts model (msgpack files)", type=pathlib.Path, nargs='+')
     #parser.add_argument('-m', '--mode', help="Visualization mode", choices=("whole_potts_model"), default="whole_potts_model")
     parser.add_argument('-1', '--start_at_1', help="Start numbering at 1", action='store_true', default=True), 
     parser.add_argument('-0', '--start_at_0', help="Start numbering at 0", action='store_true', default=False), 
@@ -16,8 +16,10 @@ def main():
 
     start_at_1 = args["start_at_1"] and not args["start_at_0"]
 
-    mrf = Potts_Model.from_msgpack(args["potts_model"])
-    visualize_mrf(mrf, start_at_1=start_at_1)
+    for msgpack in args["potts_models"]:
+        mrf = Potts_Model.from_msgpack(msgpack)
+        visualize_mrf(mrf, start_at_1=start_at_1, show_figure=False)
+    plt.show()
 
 
 
