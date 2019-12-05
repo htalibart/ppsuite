3551cf625f95f561a4e7065e07e9189456e77c23
TALIBART Hugo <hugo.talibart@irisa.fr>
2019-12-05 19:13:17 +0100

    retrieves sequences from blast database using blastdbcmd

--- comutils/blast_utils.py
+++ comutils/blast_utils.py
@@ -1,18 +1,16 @@
+import os
+from Bio import SeqIO
 from Bio.Blast.Applications import NcbiblastpCommandline
 from Bio.Blast import NCBIXML
 import pathlib
+import subprocess
 
-from comutils.db_utils import *
-
-
-
-def call_blast(fastafilename, dbpath, n=100000, blastresultsfile=None, evalue=100, remote=True):
-    # TODO test
+def call_blast(fastafilename, dbpath, n=100000, blastresultsfile=None, evalue=100):
     if blastresultsfile is None:
         blastresultsfilename = '.'.join(str(fastafilename).split('.')[:-1])+"_blast.xml"
         blastresultsfile = pathlib.Path(blastresultsfilename)
 
-    cline = NcbiblastpCommandline(query=str(fastafilename), db=dbpath, remote=remote, out=str(blastresultsfile), outfmt="5", evalue=evalue, max_target_seqs=n)
+    cline = NcbiblastpCommandline(query=str(fastafilename), db=dbpath, remote=False, out=str(blastresultsfile), outfmt="5", evalue=evalue, max_target_seqs=n)
     print('Running:', cline)
     stdout, stderr = cline()
     if stdout:
@@ -29,33 +27,44 @@ def get_hits_from_blast_xml(blast_xml):
     results = []
     with open(blast_xml) as result_handle:
         blast_records = NCBIXML.parse(result_handle)
-        for recordcount, blast_record in enumerate(blast_records):
+        for blast_record in blast_records:
             if blast_record.descriptions:
                 for desc in blast_record.descriptions:
-                    results.append(desc.title.split(' ')[0])
+                   results.append(desc.accession)
     return results
 
 
-def get_full_records_from_blast_xml(blast_xml):
-    sprot = SwissprotAccessor(sprot_filepath=None)
+def get_blast_record(uid, dbpath):
+    tmp_fasta_seq = "temp_seq.fasta"
+    cmd = "blastdbcmd -entry "+uid+" -db "+dbpath+" -target_only > "+tmp_fasta_seq
+    subprocess.Popen(cmd, shell=True).wait()
+    record = list(SeqIO.parse(tmp_fasta_seq, "fasta"))[0]
+    os.remove(tmp_fasta_seq)
+    return record
+
+
+
+def get_full_records_from_blast_xml(blast_xml, dbpath):
     results = get_hits_from_blast_xml(blast_xml)
     hit_records = []
     for hit in results:
-        uid = hit.split('|')[1]
-        try:
-            rec = sprot.get_record(uid)
-            hit_records.append(rec)
-        except Exception as e:
-            print(hit, e)
+        rec = get_blast_record(hit, dbpath)
+        hit_records.append(rec)
     return hit_records
 
 
-def get_fasta_with_full_sequences_from_blast_xml(blast_xml, blast_fasta=None):
+def get_fasta_with_full_sequences_from_blast_xml(blast_xml, dbpath, blast_fasta=None):
     if blast_fasta is None:
         blast_fasta_name = '.'.join(str(blast_xml).split('.')[:-1])+".fasta"
         blast_fasta = pathlib.Path(blast_fasta_name)
 
-    hit_records = get_full_records_from_blast_xml(blast_xml)
+    hit_records = get_full_records_from_blast_xml(blast_xml, dbpath)
     SeqIO.write(hit_records, blast_fasta, "fasta")
     
     return blast_fasta
+
+
+def get_blast_xml_and_fasta_output_from_sequence_file(sequence_file, dbpath, blast_fasta=None, blast_xml=None, n=100000, evalue=100):
+    blast_xml = call_blast(str(sequence_file), dbpath, n=n, blastresultsfile=blast_xml, evalue=evalue)
+    blast_fasta = get_fasta_with_full_sequences_from_blast_xml(blast_xml, dbpath, blast_fasta=blast_fasta)
+    return pathlib.Path(blast_xml), pathlib.Path(blast_fasta)
--- tests/test_blast_utils.py
+++ tests/test_blast_utils.py
@@ -1,4 +1,3 @@
-# TODO CHANGER LES FICHIERS DE TEST
 import unittest
 import shutil, tempfile
 import pathlib
@@ -7,28 +6,25 @@ import pkg_resources
 from tests.resources_manager import *
 
 from comutils.blast_utils import *
-
+from comutils import files_management as fm
 
 class Test_BLAST_Utils(unittest.TestCase):
+    def setUp(self):
+        self.db_path = '~/data/blastdb/swissprot'
 
     def test_get_hits_from_blast_xml(self):
         records = get_hits_from_blast_xml(BLAST_XML)
         assert(len(records)>0)
 
-    def test_get_full_records_from_blast_xml(self):
-        full_records = get_full_records_from_blast_xml(BLAST_XML)
-        assert(len(full_records)>0)
-
-    def test_get_fasta_with_full_sequences_from_blast_xml(self):
-        blast_fasta = get_fasta_with_full_sequences_from_blast_xml(BLAST_XML)
-        assert(blast_fasta.exists())
-
-    def test_call_blast(self):
+    def test_get_blast_xml_and_fasta_output_from_sequence_file(self):
         blast_xml = pathlib.Path('/tmp/'+next(tempfile._get_candidate_names()))
-        nb_seq = 10
-        call_blast(SEQ_1CC8, "nr", n=nb_seq, blastresultsfile=blast_xml, evalue=0.1, remote=True)
-        records = get_hits_from_blast_xml(blast_xml)
-        assert(len(records)==nb_seq)
+        blast_fasta = pathlib.Path('/tmp/'+next(tempfile._get_candidate_names()))
+        nb_sequences=100
+        blast_xml, blast_fasta = get_blast_xml_and_fasta_output_from_sequence_file(SEQ_1CC8, self.db_path, blast_fasta=blast_fasta, blast_xml=blast_xml, n=nb_sequences, evalue=100)
+        assert(blast_xml.exists())
+        assert(blast_fasta.exists())
+        assert(fm.get_nb_sequences_in_fasta_file(blast_fasta)==nb_sequences)
+
 
 if __name__=='__main__':
     unittest.main()
