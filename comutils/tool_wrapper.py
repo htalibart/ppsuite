448c7f64428f23677332df46456edd11ee730923
TALIBART Hugo <hugo.talibart@irisa.fr>
2020-02-03 10:53:26 +0100

    changed back to old hhblits options for consistency in xps

--- comutils/tool_wrapper.py
+++ comutils/tool_wrapper.py
@@ -18,13 +18,14 @@ def call_hhfilter(input_file, output_file, hhid):
     subprocess.Popen(cmd, shell=True).wait()
 
 
-def call_hhblits(input_file, output_file, database, n=3, e=0.001, retry_hhblits_with_memory_limit_if_fail=False, hhr_file=None, **kwargs):
+def call_hhblits(input_file, output_file, database, maxfilt=100000, realign_max=100000, B=100000, Z=100000, n=3, e=0.001, retry_hhblits_with_memory_limit_if_fail=False, hhr_file=None, **kwargs):
     """ calls HH-blits with arguments recommended for CCMpred : https://github.com/soedinglab/CCMpred/wiki/FAQ """
     if hhr_file is None:
         hhr_file = pathlib.Path('.'.join(str(output_file).split('.')[:-1])+".hhr")
     print("calling hhblits on "+str(input_file)+" using "+str(database)+", output will be available at "+str(output_file))
     fm.check_if_file_ok(input_file)
-    hhblits_call = "hhblits -d "+str(database)+" -n "+str(n)+" -e "+str(e)+" -i "+str(input_file)+" -oa3m "+str(output_file)+" -o "+str(hhr_file)
+    #hhblits_call = "hhblits -d "+str(database)+" -n "+str(n)+" -e "+str(e)+" -i "+str(input_file)+" -oa3m "+str(output_file)+" -o "+str(hhr_file)
+    hhblits_call = "hhblits -maxfilt "+str(maxfilt)+" -realign_max "+str(realign_max)+" -d "+str(database)+" -all -B "+str(B)+" -Z "+str(Z)+" -n "+str(n)+" -e "+str(e)+" -i "+str(input_file)+" -oa3m "+str(output_file)+" -o "+str(hhr_file)
     print(hhblits_call)
     subprocess.Popen(hhblits_call, shell=True).wait()
     if not output_file.exists() and retry_hhblits_with_memory_limit_if_fail:
