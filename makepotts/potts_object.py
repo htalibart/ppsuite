cb739934681e2e6f07abfbab5b99ca692d43969d
TALIBART Hugo <hugo.talibart@irisa.fr>
2020-06-29 10:31:35 +0200

    changed regularization factor back to ccmpredpy default

--- makepotts/potts_object.py
+++ makepotts/potts_object.py
@@ -92,7 +92,7 @@ class Potts_Object:
 
 
     @classmethod
-    def from_files(cls, feature_folder=None, sequence_file=None, potts_model_file=None, aln_file=None, unaligned_fasta=None, fetch_sequences=False, sequences_fetcher='hhblits', database=None, use_evalue_cutoff=False, hhr_file=None, blast_xml=None, filter_alignment=True, hhfilter_threshold=80, use_less_sequences=True, max_nb_sequences=1000, min_nb_sequences=1, trim_alignment=True, trimal_gt=0.8, trimal_cons=0, infer_potts_model=True, inference_type="standard", pc_single_count=None, reg_lambda_pair_factor=None, v_rescaling_function="identity", w_rescaling_function="identity", use_w=True, nb_sequences_blast=100000, blast_evalue=1, keep_tmp_files=False, add_pseudo_w=False, w_submat_tau=0.05, rescale_wij=False, max_potts_model_length=250, **kwargs):
+    def from_files(cls, feature_folder=None, sequence_file=None, potts_model_file=None, aln_file=None, unaligned_fasta=None, fetch_sequences=False, sequences_fetcher='hhblits', database=None, use_evalue_cutoff=False, hhr_file=None, blast_xml=None, filter_alignment=True, hhfilter_threshold=80, use_less_sequences=True, max_nb_sequences=1000, min_nb_sequences=1, trim_alignment=True, trimal_gt=0.8, trimal_cons=0, infer_potts_model=True, inference_type="standard", pc_single_count=None, reg_lambda_pair_factor=0.2, v_rescaling_function="identity", w_rescaling_function="identity", use_w=True, nb_sequences_blast=100000, blast_evalue=1, keep_tmp_files=False, add_pseudo_w=False, w_submat_tau=0.05, rescale_wij=False, max_potts_model_length=250, **kwargs):
 
         # ALIGNMENT FOLDER
         if feature_folder is None:
@@ -227,9 +227,9 @@ class Potts_Object:
                 if inference_type=="standard":
                     if pc_single_count is None:
                         pc_single_count = fm.get_nb_sequences_in_fasta_file(aln_train)
-                    if reg_lambda_pair_factor is None:
-                        L = fm.get_nb_columns_in_alignment(aln_train)
-                        reg_lambda_pair_factor = 30/(L-1)
+                    #if reg_lambda_pair_factor is None:
+                    #    L = fm.get_nb_columns_in_alignment(aln_train)
+                    #    reg_lambda_pair_factor = 30/(L-1)
                     potts_model = Potts_Model.from_training_set(aln_train, potts_model_file, pc_single_count=pc_single_count, reg_lambda_pair_factor=reg_lambda_pair_factor, **kwargs)
 
                 elif inference_type=="one_submat":
@@ -394,7 +394,7 @@ def main(args=sys.argv[1:]):
     parser.add_argument('--pc_pair_count', help="CCMpred : Specify number of pair pseudocounts (default : 1)", default=1)
     parser.add_argument('--ofn_pll', help="CCMpred : Pseudo-likelihood inference (default : True)", default=True)
     parser.add_argument('--ofn_cd', help="CCMpred : Contrastive Divergence inference (default : False)", default=False)
-    parser.add_argument('--reg_lambda_pair_factor', help="CCMpred : Regularization parameter for pair potentials (L2 regularization with lambda_pair = lambda_pair_factor * scaling) [CCMpred default: 0.2, our default : 30/(L-1)]", default=None)
+    parser.add_argument('--reg_lambda_pair_factor', help="CCMpred : Regularization parameter for pair potentials (L2 regularization with lambda_pair = lambda_pair_factor * scaling) [CCMpred default: 0.2]", default=0.2)
 
 
 
