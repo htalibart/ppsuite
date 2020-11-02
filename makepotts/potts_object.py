24879902f235ffb60809eccf12163542a541d5da
TALIBART Hugo <hugo.talibart@irisa.fr>
2020-11-02 12:14:57 +0100

    init potts model to none

--- makepotts/potts_object.py
+++ makepotts/potts_object.py
@@ -91,6 +91,8 @@ class Potts_Object:
     @classmethod
     def from_files(cls, feature_folder=None, sequence_file=None, potts_model_file=None, aln_file=None, unaligned_fasta=None, fetch_sequences=False, sequences_fetcher='hhblits', database=None, use_evalue_cutoff=False, hhr_file=None, blast_xml=None, filter_alignment=True, hhfilter_threshold=80, use_less_sequences=True, max_nb_sequences=1000, min_nb_sequences=1, trim_alignment=True, trimal_gt=0.8, trimal_cons=0, infer_potts_model=True, inference_type="standard", pc_single_count=1, reg_lambda_pair_factor=0.2, v_rescaling_function="identity", w_rescaling_function="identity", use_w=True, nb_sequences_blast=100000, blast_evalue=1, keep_tmp_files=False, max_potts_model_length=250, insert_null_at_trimmed=False, v_null_is_v0=True, **kwargs):
 
+        potts_model=None
+
         # ALIGNMENT FOLDER
         if feature_folder is None:
             folder_name = str(uuid.uuid4())
