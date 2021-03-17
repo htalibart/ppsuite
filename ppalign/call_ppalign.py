9fa0d01a95f2d91f5782bd6aa3811fde2185ab86
TALIBART Hugo <hugo.talibart@irisa.fr>
2021-03-17 12:55:32 +0100

    added comments

--- ppalign/call_ppalign.py
+++ ppalign/call_ppalign.py
@@ -14,43 +14,49 @@ PPALIGN_SOLVER = ctypes.CDLL(PPALIGN_CPP_LIBRARY)
 INFINITY = 1000000000
 
 def align_two_potts_models(mrfs, output_folder, n_limit_param=INFINITY, iter_limit_param=1000, t_limit=36000, disp_level=1, epsilon_sim=0.005, w_percent=100, use_w=True, use_v=True, gamma=1.0, theta=0.9, stepsize_min=0.000000005, nb_non_increasing_steps_max=500, alpha_w=1, gap_open=8, gap_extend=0, sim_min=0.1, offset_v=0, remove_v0=False, **kwargs):
-    
+   
+    # handle output files and folder
     if not output_folder.is_dir():
         fm.create_folder(output_folder)
-
     aln_res_file = fm.get_aln_res_file_name(output_folder)
     info_res_file = fm.get_info_res_file_name(output_folder)
 
+    # total PPalign time starts now
     time_start = time.time()
-    
-    c_float_p = ctypes.POINTER(ctypes.c_float) # defining type for ctypes : pointer to float
+  
 
-    # handle v
+   # DEFINE TYPES FOR CTYPES
+    c_float_p = ctypes.POINTER(ctypes.c_float) # pointer to float
+    c_int_p = ctypes.POINTER(ctypes.c_int) # pointer to int
+
+
+    # FIELDS
     if use_v:
-        if remove_v0:
+        if remove_v0: # remove v0 from every field if necessary
             vs = [mrf.v-np.tile(get_background_v0(**kwargs),(mrf.ncol,1)) for mrf in mrfs]
         else:
             vs = [mrf.v for mrf in mrfs]
-        v_flats = [np.ascontiguousarray(v.flatten()) for v in vs]
+        v_flats = [np.ascontiguousarray(v.flatten()) for v in vs] # flatten for ctypes
     else:
         v_flats = [np.ascontiguousarray(np.zeros(mrf.v.shape)).flatten() for mrf in mrfs]
     c_vs = [vflat.astype(np.float32).ctypes.data_as(c_float_p) for vflat in v_flats]
 
+
+    # COUPLINGS
     if use_w:
         edges_maps = [get_edges_map(mrf, w_percent) for mrf in mrfs]
         w_flats = [np.ascontiguousarray(mrf.w.flatten()) for mrf in mrfs]
-    else:
+    else: # if no coupling: edge map where edges are all 0
         edges_maps = [np.zeros((mrf.w.shape[0:2])) for mrf in mrfs]
         w_flats = [np.ascontiguousarray(np.zeros(mrf.w.shape)).flatten() for mrf in mrfs]
     c_ws = [wflat.astype(np.float32).ctypes.data_as(c_float_p) for wflat in w_flats]
-        
-    selfcomps = [compute_selfscore(mrf, edges_map, alpha_w=alpha_w, remove_v0=remove_v0, offset_v=offset_v, use_v=use_v, use_w=use_w, **kwargs) for mrf, edges_map in zip(mrfs, edges_maps)]
-    epsilon = get_epsilon(epsilon_sim, selfcomps)
+    
+    selfcomps = [compute_selfscore(mrf, edges_map, alpha_w=alpha_w, remove_v0=remove_v0, offset_v=offset_v, use_v=use_v, use_w=use_w, **kwargs) for mrf, edges_map in zip(mrfs, edges_maps)] #s(A,A) and s(B,B)
+    epsilon = get_epsilon(epsilon_sim, selfcomps) # epsilon so that 2*s(A,B)/(s(A,A)+s(B,B)) < @epsilon_sim
+    score_min = (1/2)*sim_min*sum(selfcomps); # stop computation if LB < score_min 
 
-    c_int_p = ctypes.POINTER(ctypes.c_int)
     c_edges_maps = [np.ascontiguousarray(edges_map.flatten(), dtype=np.int32).ctypes.data_as(c_int_p) for edges_map in edges_maps]
 
-    score_min = (1/2)*sim_min*sum(selfcomps); 
 
     PPALIGN_SOLVER.call_from_python.argtypes=[c_float_p, c_float_p, c_float_p, c_float_p, ctypes.c_int, ctypes.c_int, c_int_p, c_int_p, ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_char_p, ctypes.c_char_p, ctypes.c_int, ctypes.c_int, ctypes.c_double, ctypes.c_int, ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_int, ctypes.c_double, ctypes.c_double, ctypes.c_double]
 
