4a29f7e97779178ee94fc1416d25b0c8ed379069
TALIBART Hugo <hugo.talibart@irisa.fr>
2021-03-31 09:47:08 +0200

    test external gaps

--- tests/test_insertions.py
+++ tests/test_insertions.py
@@ -67,21 +67,22 @@ class Test_Insertions(unittest.TestCase):
         aligned_positions, infos_solver = align_two_potts_models(mrfs, self.output_folder, insert_costs=insert_costs, sim_min=-100, epsilon_sim=0.0001)
         self.assertEqual(aligned_positions, expected_aligned_positions)
 
+    def test_no_external_cost(self):
+        nb_before = 50
+        nb_after = 50
+        internal_open_1=100
+        internal_extend_1=10
+        internal_open_2=100
+        internal_extend_2=10
+        mrfs = [get_fake_model([0]*nb_before+[1,2]+[0]*nb_after), get_fake_model([1,2])]
+        insert_costs = [{"open":[0]+[internal_open_1]*(nb_before+2+nb_after-1)+[0], "extend":[0]+[internal_extend_1]*(nb_before+2+nb_after-1)+[0]},
+                        {"open":[0,internal_open_2,0], "extend":[0,internal_extend_2,0]}
+                        ]
+        expected_aligned_positions = {"pos_ref":[nb_before,nb_before+1], "pos_2":[0,1]}
+        aligned_positions, infos_solver = align_two_potts_models(mrfs, self.output_folder, insert_costs=insert_costs, sim_min=-100, epsilon_sim=0.0001)
+        print(aligned_positions)
+        self.assertEqual(aligned_positions, expected_aligned_positions)
 
-
-
-
-#    def test_costly_open_with_templates(self):
-#        templates = [['y','y','y'],['[0]', 'y', '[1]', '[2]']]
-#        insert_costs = [{"open":[0,1000,0,0],"extend":[0,0,0,0]}, {"open":[0,0,0,0,0],"extend":[0,0,0,0,0]}]
-#        expected_aligned_positions = {"pos_ref":[1,2], "pos_2":[2,3]}
-#        alnfnames = [self.output_folder/("fake_"+str(i)+".aln") for i in range(2)]
-#        fastafnames = [self.output_folder/("fake_"+str(i)+".fasta") for i in range(2)]
-#        crfake.main(templates, alnfnames, fastafnames, n=500)
-#        mrfs = [Potts_Model.from_training_set(fastafnames[i], self.output_folder/("fake_"+str(i)+".mrf")) for i in range(2)]
-#        aligned_positions, infos_solver = align_two_potts_models(mrfs, self.output_folder, insert_costs=insert_costs, sim_min=-100, epsilon_sim=0.0001)
-#        self.assertEqual(aligned_positions, expected_aligned_positions)
-#
 
 if __name__=='__main__':
     unittest.main()
