4da5386663ba088fc6317a1b7ca2673dc5cb6f53
TALIBART Hugo <hugo.talibart@irisa.fr>
2021-04-01 08:35:42 +0200

    set external insertion penalties to 0 directly in file

--- makepotts/handle_insertions.py
+++ makepotts/handle_insertions.py
@@ -11,11 +11,6 @@ from Bio.Alphabet import IUPAC
 import pkg_resources
 julia_script_insertions_file = pkg_resources.resource_filename('makepotts', 'infer_insertion_penalties.jl')
 
-def infer_insertion_penalties_in_file(seed_a3m_file, seed_length, output_file):
-    fm.check_if_file_ok(seed_a3m_file)
-    call_dcabuild_infer_ins = Main.include(julia_script_insertions_file)
-    call_dcabuild_infer_ins(str(seed_a3m_file), seed_length, str(output_file))
-
 
 
 def get_insertion_penalties_from_file(insertion_penalties_file):
@@ -29,12 +24,31 @@ def get_insertion_penalties_from_file(insertion_penalties_file):
             insertion_penalties['open'].append(float(row[0]))
             insertion_penalties['extend'].append(float(row[1]))
 
-    # external gaps:
+    return insertion_penalties
+
+
+def write_insertion_penalties_in_file(insertion_penalties, output_file):
+    nb_inser = len(insertion_penalties['open'])
+    with open(str(output_file), 'w') as tsv_file:
+        csv_writer = csv.writer(tsv_file, delimiter='\t')
+        for i in range(nb_inser):
+            csv_writer.writerow([insertion_penalties['open'][i], insertion_penalties['extend'][i]])
+
+
+
+def infer_insertion_penalties_in_file(seed_a3m_file, seed_length, output_file):
+    fm.check_if_file_ok(seed_a3m_file)
+    call_dcabuild_infer_ins = Main.include(julia_script_insertions_file)
+    call_dcabuild_infer_ins(str(seed_a3m_file), seed_length, str(output_file))
+    insertion_penalties = get_insertion_penalties_from_file(output_file)
+    # set external insertions to 0
     for insertion_type in ['open', 'extend']:
         insertion_penalties[insertion_type][0] = 0
         insertion_penalties[insertion_type].append(0)
+    write_insertion_penalties_in_file(insertion_penalties, output_file)
 
-    return insertion_penalties
+
+
 
 
 
